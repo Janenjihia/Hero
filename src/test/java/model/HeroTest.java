002ee73e27c535aacda59543b445ffda93a7c4e0
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-02 16:40:18 +0300

    Add test for verifying creation of new hero instance

--- src/test/java/model/HeroTest.java
+++ src/test/java/model/HeroTest.java
@@ -2,16 +2,17 @@ package model;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HeroTest {
-
-    @Before
-    public void setUp() throws Exception {
+    @Test
+    public void NewHeroGetsCorrectlyCreated_true() throws Exception {
+      Hero hero = new Hero();
+        assertEquals(true, hero instanceof Hero);
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
+
+
 }
