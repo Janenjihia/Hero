2ea3bca94f5b494897ee757076e20bae897b1a82
janenjihia <jane.njihia@student.moringaschool.com>
2022-05-02 17:34:14 +0300

    Add test for getting hero age and strength with the respective methods

--- src/main/java/model/Hero.java
+++ src/main/java/model/Hero.java
@@ -2,9 +2,37 @@ package model;
 
 public class Hero {
     private String heroName;
+    private int Age;
+    private String strength;
+    private String Weakness;
+    private int Id;
+
+
+
+    public Hero(String heroName, int Age, String strength, String Weakness, int Id) {
+        this.heroName=heroName;
+        this.Age=Age;
+        this.Weakness=Weakness;
+        this.Id=Id;
+        this.strength=strength;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+
 
 
     public String getHeroName() {
         return heroName;
+    }
+
+    public int getHeroAge() {
+        return Age;
+    }
+
+    public String getHeroStrength() {
+        return strength;
     }
 }
--- src/test/java/model/HeroTest.java
+++ src/test/java/model/HeroTest.java
@@ -1,24 +1,37 @@
 package model;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HeroTest {
 
-    Hero hero= new Hero();
+//    Test for ability to instantiate new hero
+Hero hero = new Hero("jane", 25, "Empathy", "kindness", 1);
     @Test
     public void NewHeroGetsCorrectlyCreated_true() throws Exception {
-      Hero hero = new Hero();
-        assertEquals(true, hero instanceof Hero);
+        
+        Hero hero= new Hero("jane", 25, "Empathy", "Kindness", 1);
+        assertEquals(true, true);
     }
 
-@Test
+//    test to get hero name
+     @Test
      public void getHeroName (){
-    assertEquals("Barry", hero.getHeroName());
-}
-
+        assertEquals("jane", hero.getHeroName());
+     }
+     
+//    test to get hero age
+     @Test
+     public void getAge(){
+         assertEquals(25, hero.getHeroAge());
+     }
+    //    test to get hero strength
+    @Test
+    public void getStrength(){
+        assertEquals("Empathy", hero.getHeroStrength());
+    }
+    //    test to get hero weakness
+    //    test to get hero id
 
 }
